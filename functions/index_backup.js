c2bcbc4fb5b33b1858ae641a99e3ed17ecd99e27
jeroen de Bruin <j.debruin@e-village.nl>
2022-03-17 13:13:13 +0100

    main: Remove test files

--- functions/index_backup.js
+++
@@ -1,9 +0,0 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-})
-
-async function handleRequest(request) {
-  return new Response('... bikingperu.com ...\nAwesome!', {
-    headers: { 'content-type': 'text/plain' },
-  })
-}
