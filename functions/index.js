38028c698e1a73664758fffd4954b7ea9f7acdaf
jeroen de Bruin <j.debruin@e-village.nl>
2022-03-17 13:11:49 +0100

    main: All js now

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,17 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request));
-})
+export async function onRequest(context) {
+  // Contents of context object
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
 
-async function handleRequest(request) {
+  console.log(context);
+
   return new Response('... bikingperu.com ...\nAwesome!', {
     headers: { 'content-type': 'text/plain' },
-  })
+  });
 }
