d2ff981d3515b7ce1b011a1e997a130a2e4226e1
jeroen de Bruin <j.debruin@e-village.nl>
2022-03-22 14:18:21 +0100

    main: Add Timestamp and ProcessTime to header (index page)

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,10 @@ export async function onRequest(context) {
     return new Response(JSON.stringify({
         status: "OK"
     }), {
-        headers: { 'content-type': 'application/json;charset=UTF-8' }
+        headers: {
+            'content-type': 'application/json;charset=UTF-8',
+            'X-Timestamp':context.data.timestamp,
+            'X-ProcessTime': Date.now() - context.data.timestamp
+        }
     });
 }
