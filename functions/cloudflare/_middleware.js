cd64af91ac98e3966fa4a87ef47aa3845a2e2ae2
Jeroen de Bruin <jeroen@jeronimodesign.com>
2022-04-16 10:55:27 +0200

    Get user details from cloudflare api

--- functions/cloudflare/_middleware.js
+++ functions/cloudflare/_middleware.js
@@ -1,13 +1,13 @@
 async function getApiKey(context) {
     try {
-      context.data.apikey = 'some_secret_key';
+        // todo
   
-      // wait for the next function to finish
-      return await context.next();
+        // wait for the next function to finish
+        return await context.next();
     } catch (err) {
-      // catch and report and errors when running the next function
-      return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+        // catch and report and errors when running the next function
+        return new Response(`${err.message}\n${err.stack}`, { status: 500 });
     }
-  }
+}
   
-  export const onRequest = [getApiKey];
+export const onRequest = [getApiKey];
--- functions/cloudflare/getUserDetails.js
+++ functions/cloudflare/getUserDetails.js
@@ -1,19 +1,44 @@
+// https://developers.cloudflare.com/workers/examples/fetch-json/
+async function gatherResponse(response) {
+    const { headers } = response;
+    const contentType = headers.get('content-type') || '';
+
+    if (contentType.includes('application/json')) {
+        return JSON.stringify(await response.json());
+    } else if (contentType.includes('application/text')) {
+        return response.text();
+    } else if (contentType.includes('text/html')) {
+        return response.text();
+    } else {
+        return response.text();
+    }
+}
+  
+
 export async function onRequest(context) {
+    if (!context.env.TOKEN_USER_DETAILS_READ.length) {
+        throw 'no valid token given';
+    }
+    
     const url = 'https://api.cloudflare.com/client/v4/user',
-        token = context.env.TOKEN_USER_DETAILS_READ;
+        init = {
+            headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Authorization': 'Bearer ' + context.env.TOKEN_USER_DETAILS_READ
+            },
+        },
+        results = await gatherResponse(response);
+
+    const response = await fetch(url, init);
 
     return new Response(JSON.stringify({
         status: "OK",
-        data: {
-            userDetails: 'Coming soon...',
-            token: token.substr(-2, 2)
-        }
+        data: results
     }), {
         headers: { 
             'content-type': 'application/json;charset=UTF-8',
             'X-Timestamp':context.data.timestamp,
-            'X-ProcessTime': Date.now() - context.data.timestamp,
-            'X-ApiKey': context.data.apikey
+            'X-ProcessTime': Date.now() - context.data.timestamp
         }
     });
 }
