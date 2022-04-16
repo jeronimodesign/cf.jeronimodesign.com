b81e19617a91ac521dfc0291210287f7f04d34bf
Jeroen de Bruin <jeroen@jeronimodesign.com>
2022-04-16 10:35:01 +0200

    Read token

--- functions/cloudflare/getUserDetails.js
+++ functions/cloudflare/getUserDetails.js
@@ -1,8 +1,12 @@
 export async function onRequest(context) {
+    const url = 'https://api.cloudflare.com/client/v4/user',
+        token = TOKEN_USER_DETAILS_READ;
+
     return new Response(JSON.stringify({
         status: "OK",
         data: {
-            userDetails: 'Coming soon...'
+            userDetails: 'Coming soon...',
+            token: token.substr(-2, 2)
         }
     }), {
         headers: { 
