78449c90c6bba1fd6499a34963c529139f5c11a1
jeroen de Bruin <j.debruin@e-village.nl>
2022-04-20 15:35:43 +0200

    main: Get dns record information

--- functions/cloudflare/updateJeronimoDesignNetPiZones.js
+++ functions/cloudflare/updateJeronimoDesignNetPiZones.js
@@ -17,7 +17,7 @@ async function gatherResponse(response) {
     }
 }
 
-async function getZoneInfo(context) {
+async function getZoneId(context) {
     if (!context.env.TOKEN_ZONE_ZONE_JERONIMODESIGN_NET_READ.length) {
         throw 'no valid token given';
     }
@@ -32,14 +32,48 @@ async function getZoneInfo(context) {
 
     let url = new URL(zoneBaseUrl);
 
-    url.searchParams.append('type', type);
+    url.searchParams.append('match', 'any');
     url.searchParams.append('name', domain);
     url.searchParams.append('status', 'active');
-    url.searchParams.append('match', 'any');
+    url.searchParams.append('type', type);
 
     const response = await fetch(url.href, init);
 
-    return await gatherResponse(response);
+    const results = JSON.parse(await gatherResponse(response));
+    if (results.success !== true) {
+        throw 'cannot get zone information'
+    }
+
+    return results.result[0].id;
+}
+
+async function getDNSRecordId(context, zoneId, name) {
+    if (!context.env.TOKEN_ZONE_JERONIMODESIGN_NET_EDIT.length) {
+        throw 'no valid token given';
+    }
+
+    const init = {
+            method: 'GET',
+            headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    'Authorization': 'Bearer ' + context.env.TOKEN_ZONE_ZONE_JERONIMODESIGN_NET_READ
+                }
+            }
+
+    let url = new URL(zoneBaseUrl + '/' + zoneId + 'dns_records');
+
+    url.searchParams.append('match', 'any');
+    url.searchParams.append('name', name + '.' + domain);
+    url.searchParams.append('type', type);
+
+    const response = await fetch(url.href, init);
+
+    const results = JSON.parse(await gatherResponse(response));
+    if (results.success !== true) {
+        throw 'cannot get dns record information'
+    }
+
+    return results; 
 }
 
 export async function onRequest(context) {
@@ -47,7 +81,17 @@ export async function onRequest(context) {
         throw 'no valid token given';
     }
 
-    const results = await getZoneInfo(context);
+    const records = [
+        'pi',
+        'pi3',
+        'bitwarden',
+    ];
+
+    const zoneId = await getZoneId(context);
+
+    const dnsRecordId = await getDNSRecordId(context, zoneId, records[0]);
+
+
     
     // const url = 'https://api.cloudflare.com/client/v4/zones',
     //     init = {
@@ -66,7 +110,9 @@ export async function onRequest(context) {
 
     return new Response(JSON.stringify({
         status: "OK",
-        data: JSON.parse(results)
+        zoneId: zoneId,
+        dnsRecordId: dnsRecordId,
+        // data: JSON.parse(zoneId)
     }), {
         headers: { 
             'content-type': 'application/json;charset=UTF-8',
