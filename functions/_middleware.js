d7c8ea2a1bfee38cabee924216c4464bc6781262
Jeroen de Bruin <jeroen@jeronimodesign.com>
2022-04-20 18:46:38 +0200

    Debug

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -7,7 +7,8 @@ async function errorHandler(context) {
     return await context.next();
   } catch (err) {
     // catch and report and errors when running the next function
-    return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+    // return new Response(`${err.message}\n${err.stack}`, { status: 500 });
+    return new Response(err.message, { status: 500 });
   }
 }
 
