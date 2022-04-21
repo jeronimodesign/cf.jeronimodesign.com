c4ad1685512d110e42cc14786a2c237cf0e749fe
jeroen de Bruin <j.debruin@e-village.nl>
2022-04-21 18:26:52 +0200

    main: Moved methods to separate file

---
+++ functions/cloudflare/cloudflare.js
@@ -0,0 +1,76 @@
+const zoneBaseUrl = 'https://api.cloudflare.com/client/v4/zones';
+
+export async function getZone(context, domain) {
+    if (!context.env.TOKEN_ZONE_ZONE_JERONIMODESIGN_NET_READ.length) {
+        throw 'no valid token given';
+    }
+
+    const init = {
+            method: 'GET',
+            headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    'Authorization': 'Bearer ' + context.env.TOKEN_ZONE_ZONE_JERONIMODESIGN_NET_READ
+                }
+            }
+
+    let url = new URL(zoneBaseUrl);
+
+    url.searchParams.append('match', 'all');
+    url.searchParams.append('name', domain);
+    url.searchParams.append('status', 'active');
+
+    const response = await fetch(url.href, init);
+
+    const results = JSON.parse(await gatherResponse(response));
+    if (results.success !== true || results.result.length < 1) {
+        throw 'cannot get zone information'
+    }
+
+    return results;
+}
+
+export async function getDNSRecord(context, zoneId, name) {
+    if (!context.env.TOKEN_ZONE_JERONIMODESIGN_NET_EDIT.length) {
+        throw 'no valid token given';
+    }
+
+    const init = {
+            method: 'GET',
+            headers: {
+                    'Content-Type': 'application/json;charset=UTF-8',
+                    'Authorization': 'Bearer ' + context.env.TOKEN_ZONE_JERONIMODESIGN_NET_EDIT
+                }
+            }
+
+    let url = new URL(zoneBaseUrl + '/' + zoneId + '/dns_records');
+
+    url.searchParams.append('match', 'all');
+    url.searchParams.append('name', name);
+    url.searchParams.append('type', type);
+
+    const response = await fetch(url.href, init);
+
+    const results = JSON.parse(await gatherResponse(response));
+    if (results.success !== true) {
+        throw 'cannot get dns record information'
+    }
+
+    if (results.result.length < 1) {
+        return null;
+    }
+
+    return results;
+}
+
+
+export async function getZoneId(context, domain) {
+    const zone = getZone(context, domain);
+
+    return zone.result[0].id;
+}
+
+export async function getDNSRecordId(context, zoneId, domain) {
+    const dnsRecord = getDNSRecord(context, zoneId, domain);
+
+    return dnsRecord.result[0].id;
+}
--- functions/cloudflare/updateJeronimoDesignNetPiZones.js
+++ functions/cloudflare/updateJeronimoDesignNetPiZones.js
@@ -1,83 +1,10 @@
+import { gatherResponse } from './../util.js';
+import { getDNSRecordId } from './cloudflare.js';
+import { getZoneId } from './cloudflare.js';
+
 const zoneBaseUrl = 'https://api.cloudflare.com/client/v4/zones',
     domain = 'jeronimodesign.net',
     type = 'A';
-
-async function gatherResponse(response) {
-    const { headers } = response;
-    const contentType = headers.get('content-type') || '';
-
-    if (contentType.includes('application/json')) {
-        return JSON.stringify(await response.json());
-    } else if (contentType.includes('application/text')) {
-        return response.text();
-    } else if (contentType.includes('text/html')) {
-        return response.text();
-    } else {
-        return response.text();
-    }
-}
-
-async function getZoneId(context) {
-    if (!context.env.TOKEN_ZONE_ZONE_JERONIMODESIGN_NET_READ.length) {
-        throw 'no valid token given';
-    }
-
-    const init = {
-            method: 'GET',
-            headers: {
-                    'Content-Type': 'application/json;charset=UTF-8',
-                    'Authorization': 'Bearer ' + context.env.TOKEN_ZONE_ZONE_JERONIMODESIGN_NET_READ
-                }
-            }
-
-    let url = new URL(zoneBaseUrl);
-
-    url.searchParams.append('match', 'all');
-    url.searchParams.append('name', domain);
-    url.searchParams.append('status', 'active');
-
-    const response = await fetch(url.href, init);
-
-    const results = JSON.parse(await gatherResponse(response));
-    if (results.success !== true || results.result.length < 1) {
-        throw 'cannot get zone information'
-    }
-
-    return results.result[0].id;
-}
-
-async function getDNSRecordId(context, zoneId, name) {
-    if (!context.env.TOKEN_ZONE_JERONIMODESIGN_NET_EDIT.length) {
-        throw 'no valid token given';
-    }
-
-    const init = {
-            method: 'GET',
-            headers: {
-                    'Content-Type': 'application/json;charset=UTF-8',
-                    'Authorization': 'Bearer ' + context.env.TOKEN_ZONE_JERONIMODESIGN_NET_EDIT
-                }
-            }
-
-    let url = new URL(zoneBaseUrl + '/' + zoneId + '/dns_records');
-
-    url.searchParams.append('match', 'all');
-    url.searchParams.append('name', name + '.' + domain);
-    url.searchParams.append('type', type);
-
-    const response = await fetch(url.href, init);
-
-    const results = JSON.parse(await gatherResponse(response));
-    if (results.success !== true) {
-        throw 'cannot get dns record information'
-    }
-
-    if (results.result.length < 1) {
-        return null;
-    }
-
-    return results.result[0].id;
-}
 
 async function updateDNSRecord(context, zoneId, dnsRecordId) {
     if (!context.env.TOKEN_ZONE_JERONIMODESIGN_NET_EDIT.length) {
@@ -116,23 +43,24 @@ export async function onRequest(context) {
     }
 
     const records = [
-        'pi',
-    ];
+            'pi',
+        ],
+        domain = 'jeronimodesign.net';
 
-    const zoneId = await getZoneId(context);
+    const zoneId = await getZoneId(context, domain);
 
     let dnsRecordIds = [],
         data = [];
 
     for (let i = 0; i < records.length; i++) {
-        const dnsRecordId = await getDNSRecordId(context, zoneId, records[i]);
+        const dnsRecordId = await getDNSRecordId(context, zoneId, records[i] + '.' + domain);
         if (!dnsRecordId) {
             continue;
         }
 
         dnsRecordIds.push(dnsRecordId);
 
-        data.push(await updateDNSRecord(context, zoneId, dnsRecordId));
+        // data.push(await updateDNSRecord(context, zoneId, dnsRecordId));
     }
 
     return new Response(JSON.stringify({
