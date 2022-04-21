b84fad2f120b835a2fdcc9db13142d3e1b2f5a71
jeroen de Bruin <j.debruin@e-village.nl>
2022-04-21 21:51:37 +0200

    main: Moved getUserDetails to cloudflare.js

--- functions/cloudflare/getUserDetails.js
+++ functions/cloudflare/getUserDetails.js
@@ -1,27 +1,11 @@
 import { getUserDetails } from './cloudflare.js';
-import { gatherResponse } from './../util.js';
 
 export async function onRequest(context) {
     const userDetails = await getUserDetails(context);
 
-    if (!context.env.TOKEN_USER_DETAILS_READ.length) {
-        throw 'no valid token given';
-    }
-    
-    const url = 'https://api.cloudflare.com/client/v4/user',
-        init = {
-            headers: {
-            'Content-Type': 'application/json;charset=UTF-8',
-            'Authorization': 'Bearer ' + context.env.TOKEN_USER_DETAILS_READ
-            },
-        },
-        response = await fetch(url, init),
-        results = await gatherResponse(response);
-
     return new Response(JSON.stringify({
         status: "OK",
-        data: JSON.parse(results),
-        userDetails: userDetails,
+        data: userDetails,
     }), {
         headers: { 
             'content-type': 'application/json;charset=UTF-8',
